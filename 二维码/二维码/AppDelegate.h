5caff9e7fcd7fb44304c764b5149a450fb46e526
zhangyide <zhangyide@9fbank.cc>
2018-08-29 11:08:40 +0800

    第一次提交

---
+++ 二维码/二维码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  二维码
+//
+//  Created by yidezhang on 2018/8/22.
+//  Copyright © 2018年 yidezhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 二维码/二维码/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  二维码
+//
+//  Created by yidezhang on 2018/8/22.
+//  Copyright © 2018年 yidezhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
