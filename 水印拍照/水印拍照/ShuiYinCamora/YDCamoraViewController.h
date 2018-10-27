7f097504ade2b895b88c7544c0916d33d7103357
zhangyide <zhangyide@9fbank.cc>
2018-10-27 13:22:37 +0800

    添加水印相机

---
+++ 水印拍照/水印拍照/ShuiYinCamora/YDCamoraViewController.h
@@ -0,0 +1,13 @@
+//
+//  YDCamoraViewController.h
+//  水印拍照
+//
+//  Created by yide zhang on 2018/10/27.
+//  Copyright © 2018年 yide zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YDCamoraViewController : UIViewController
+
+@end
---
+++ 水印拍照/水印拍照/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  水印拍照
+//
+//  Created by yide zhang on 2018/10/27.
+//  Copyright © 2018年 yide zhang. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "YDCamoraViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(100, 100, 100, 100);
+    [btn setTitle:@"打开相机" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.view addSubview:btn];
+    [btn addTarget:self action:@selector(btnAction) forControlEvents:UIControlEventTouchUpInside];
+}
+
+-(void)btnAction
+{
+    YDCamoraViewController *camora = [[YDCamoraViewController alloc] init];
+    [self presentViewController:camora animated:YES completion:nil];
+}
+@end
